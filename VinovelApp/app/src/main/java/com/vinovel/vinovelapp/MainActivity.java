e378d654e7867149910a75c0737950399f8a72a7
hallower <hallower@gmail.com>
2015-02-12 00:06:25 +0900

    - Add javascript interface - Turn on weview's local storage

--- VinovelApp/app/src/main/java/com/vinovel/vinovelapp/MainActivity.java
+++ VinovelApp/app/src/main/java/com/vinovel/vinovelapp/MainActivity.java
@@ -5,24 +5,23 @@ import com.vinovel.vinovelapp.util.SystemUiHider;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebStorage;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.lang.reflect.InvocationTargetException;
 
-
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class MainActivity extends Activity {
 
     private static String LOG_TAG = "vnv";
@@ -38,8 +37,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //Log.i(LOG_TAG, "onCreate!!");
         setContentView(R.layout.activity_main);
+        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         llSplash = (LinearLayout)findViewById(R.id.splash_logo);
         webview = (WebView) findViewById(R.id.fullscreen_content);
@@ -53,9 +52,12 @@ public class MainActivity extends Activity {
         handler = new Handler();
         handler.postDelayed(runnable, 8000);
 
-
-        webview.getSettings().setJavaScriptEnabled(true);
+        WebSettings settings = webview.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
         webview.setWebViewClient(new WebViewClientClass());
+        webview.addJavascriptInterface(new WebAppInterface(this), "vnv_js_api");
 
         SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
         String lastURL = pref.getString(ID_LAST_URL, DEFAULT_PAGE_URL);
@@ -63,28 +65,22 @@ public class MainActivity extends Activity {
         webview.loadUrl(lastURL);
     }
 
-    private class WebViewClientClass extends WebViewClient {
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            // TODO : limit webview's content to show only vinovel.com
-            view.loadUrl(url);
-            return true;
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            llSplash.setVisibility(View.GONE);
-        }
-    }
-
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(LOG_TAG, "onPause!! " + webview.getUrl());
+        String currentURL = webview.getUrl();
+        Log.i(LOG_TAG, "onPause!! " + currentURL);
 
         SharedPreferences pref = getSharedPreferences("pref", MODE_PRIVATE);
-        pref.edit().putString(ID_LAST_URL, webview.getUrl()).commit();
+        pref.edit().putString(ID_LAST_URL, currentURL).commit();
 
+        if(currentURL.contains("episode")){
+            if (webview.canGoBack()) {
+                webview.goBack();
+            }else{
+                webview.loadUrl(DEFAULT_PAGE_URL);
+            }
+        }
         webview.onPause();
 
         // TODO : method 1, change to blank page to mute every thing
@@ -109,7 +105,7 @@ public class MainActivity extends Activity {
     @Override
     public void onBackPressed() {
 
-        Log.i(LOG_TAG, "url = " + webview.getUrl());
+        //Log.i(LOG_TAG, "url = " + webview.getUrl());
 
         if(0 == DEFAULT_PAGE_URL.compareTo(webview.getUrl())) {
             super.onBackPressed();
@@ -124,4 +120,40 @@ public class MainActivity extends Activity {
         webview.loadUrl(DEFAULT_PAGE_URL);
     }
 
+    private class WebViewClientClass extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            // TODO : limit webview's content to show only vinovel.com
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            llSplash.setVisibility(View.GONE);
+        }
+    }
+
+    private class WebAppInterface {
+        Context mContext;
+
+        WebAppInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+        }
+
+        @JavascriptInterface
+        public void setPortraitLayout() {
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+
+        @JavascriptInterface
+        public void setLandscapeLayout() {
+            setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
+    }
 }
